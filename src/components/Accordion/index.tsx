69ccb72ea0dfa5fda4a15ebf38b668d0237885f3
김하연 <hayeonkim226@gmail.com>
2024-03-05 01:20:33 +0900

    fix: fix accordion

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -9,7 +9,7 @@ export interface AccordionProps {
 
 const Accordion = ({ rightIcon, title, content, id }: AccordionProps) => {
   return (
-    <div className="flex flex-wrap justify-start overflow-hidden text-gray-800 border-b border-gray-500">
+    <div className="flex flex-wrap justify-start overflow-hidden text-gray-800 border-b border-gray-100 bg-white">
       <label className="grow px-4 py-3 font-medium" htmlFor={id}>
         {title}
       </label>
@@ -24,18 +24,18 @@ const Accordion = ({ rightIcon, title, content, id }: AccordionProps) => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="mx-4 my-3 h-6 w-6 transition-all duration-500 peer-checked:rotate-45"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M12 6v12m6-6H6"
           />
         </svg>
       )}
-      <div className="-transparent absolute -translate-y-full scale-25 scale-y-0 px-4 py-3 opacity-0 transition-all duration-500 peer-checked:relative peer-checked:translate-y-0 peer-checked:scale-100 peer-checked:scale-y-100 peer-checked:bg-gray-50 peer-checked:opacity-100 text-sm">
+      <div className="-transparent absolute -translate-y-1/2 scale-50 scale-y-0 px-4 py-3 opacity-0 transition-all duration-500 peer-checked:relative peer-checked:translate-y-0 peer-checked:scale-100 peer-checked:scale-y-100 peer-checked:bg-gray-100 peer-checked:opacity-100 text-sm">
         {content}
       </div>
     </div>
